bf01d3597dc3b272014e7d521ae20250615608b1
Chinmay Pant <64401853+chinmaypant21@users.noreply.github.com>
2021-09-29 20:05:29 +0530

    Q6

---
+++ Q6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void TOH(int tower_size,int start, int destination)
+{
+    if (tower_size == 1)
+    {
+        cout<<"From "<<start<<" -> "<<destination<<endl;
+        return;
+    }
+
+    int middle = 6 - (start + destination);
+    TOH(tower_size-1,start,middle);
+    cout<<"From "<<start<<" -> "<<destination<<endl;
+    TOH(tower_size-1,middle,destination);
+}
+
+int main()
+{
+    int tower_size = 3;
+    TOH(tower_size,1,3);
+}
